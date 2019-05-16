38c7c328faec6aec20e806d5e0e88d57ee3cc08e
p_hyunwoo <winner3024@youremail.com>
2019-05-16 17:11:05 +0900

    test

--- 新人教育/ShainInfo/ShainInof/ActiveTBL.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class ActiveTBL
-
-End Class
--- 新人教育/ShainInfo/ShainInof/ShainMaster.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class ShainMaster
-    Private Sub ShainMaster_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-End Class
